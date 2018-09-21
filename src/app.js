f6b6507b563801024e26a6b4405ee7ed51b81752
Kairo Lamarca <lamarca.kairo@gmail.com>
2018-09-20 21:38:41 -0300

    middleware de configuração e rotas de erros
    acces control e rotas de erro 404 e 500

--- src/app.js
+++ src/app.js
@@ -2,13 +2,36 @@ const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
 
+//body parser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+//Configuração de acesso
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");//permite acesso para qualquer aplicação(dominio)
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");//Configurar metodos que a origem pode requisitar
+    res.setHeader("Access-Control-Allow-Headers", "content-type");//Habilitar que a req da origem tenha cabeçalhos reescritos
+    res.setHeader("Access-Control-Allow-Credentials", true);//
+
+    next();
+})
 
 //Rotas
 const index = require('./routes/index');
 const personRoute = require('./routes/personRoute');
 app.use('/', index);
 app.use('/persons', personRoute);
+
+/* middleware que configura páginas de status */
+app.use((req, res, next) => {
+    res.status(404).send('Recurso não encontrada');//Envia isso quando der status 404
+    next();
+})
+
+/* middleware que configura msg de erro internos*/
+app.use((err, req, res, next) => {
+    res.status(500).send('Ocorreu um erro interno, tente novamente');
+    next();
+})
 
 module.exports = app;
