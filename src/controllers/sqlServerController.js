9cbeb27451d0c20201619c90683a7585a9721b59
Kairo Lamarca <lamarca.kairo@gmail.com>
2018-10-16 22:01:54 -0300

    alteração no retorno do sql server
    alteração no retorno do sql server

--- src/controllers/sqlServerController.js
+++ src/controllers/sqlServerController.js
@@ -7,7 +7,6 @@ exports.getAll = async (req, res) => {
         res.status(200).json(result);
     }
     catch (error) {
-        console.log('erro: ', error);
         res.status(500).json(error);
     }
 }
--- src/models/sqlServerRepository.js
+++ src/models/sqlServerRepository.js
@@ -2,12 +2,9 @@ const sql = require('../config/sqlServerConnection');
 
 exports.getAll = async () => {
     //abre a conexão
-    console.log('1');
     const connection = await sql();
-    console.log('2');
-    const results = await connection.request().query('select * from usuarios');
-    console.log('3');
 
-    console.log('results', results);
-    return results;
+    const results = await connection.request().query('select * from usuarios');
+
+    return results.recordset;
 }
