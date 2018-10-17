3fcac852bc7dd4ea25809a65b245c8885417d4c6
Kairo Lamarca Santos <lamarca.kairo@gmail.com>
2018-10-17 11:09:15 -0300

    fechando a conexão do sql server
    fechando a conexão do sql server

--- src/models/sqlServerRepository.js
+++ src/models/sqlServerRepository.js
@@ -1,10 +1,14 @@
-const sql = require('../config/sqlServerConnection');
+const conn = require('../config/sqlServerConnection');
+const sql = require('mssql');
 
 exports.getAll = async () => {
     //abre a conexão
-    const connection = await sql();
+    const connection = await conn();
 
     const results = await connection.request().query('select * from usuarios');
+
+    //fecha a conexão
+    sql.close();
 
     return results.recordset;
 }
