7a44d4fb2bd9a5b22c87816f27df90e0f3596dab
Kairo Lamarca Santos <lamarca.kairo@gmail.com>
2018-10-17 10:58:42 -0300

    encrypt padrão no sql server
    encrypt padrão no sql server

--- src/config/sqlServerConnection.js
+++ src/config/sqlServerConnection.js
@@ -5,7 +5,10 @@ const config = {
     user: process.env.SQLSRV_USER,
     password: process.env.SQLSRV_PASSWORD,
     server: process.env.SQLSRV_SERVER,
-    database: process.env.SQLSRV_DATABASE
+    database: process.env.SQLSRV_DATABASE, 
+    options: {
+        encrypt: true // Use this if you're on Windows Azure
+    }
 }
 
 const connection = async () => {    
